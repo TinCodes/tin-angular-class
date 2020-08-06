7c7f6ac55ad532a9fe0038f158c07d0c8bf23d1d
Tincho <tincholaguna37@gmail.com>
2020-08-06 19:40:18 -0400

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
+import { Test1Component } from './components/test1/test1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule
