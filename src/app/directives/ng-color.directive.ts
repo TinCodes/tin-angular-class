af0d86aa9fbdfda3167d9a784a2d5739cf31eafe
Tincho <tincholaguna37@gmail.com>
2020-08-17 16:02:33 -0400

    Using HostListener on directive onColor to listen to DOM events

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,15 +1,34 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, Output, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[ngColor]'
 })
 export class NgColorDirective {
 
-  constructor(el: ElementRef) { 
+	@Input()
+	@Output()
+
+	@HostListener('mouseenter') onMouseEnter(){
+		console.log('Ow');
+
+		this.changeColor('red')
+	}
+
+	@HostListener('mouseleave') onMouseLeave(){
+		console.log('Thx');
+
+		this.changeColor('yellow');
+	}
+
+  constructor(private el: ElementRef) { 
   	console.log("Current Element: ", el);
-  	
-  	el.nativeElement.style.border = '3px solid cyan';
-  	el.nativeElement.style.borderRadius = '5px';
+
+  	this.changeColor('cyan');
+  }
+
+  changeColor(color: string) {
+  	this.el.nativeElement.style.border = '3px solid ' + color;
+  	this.el.nativeElement.style.borderRadius = '5px';
   }
 
 }
