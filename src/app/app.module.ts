6fe911ff45296852d0ae214a5cb45103b9985bb2
Tincho <tincholaguna37@gmail.com>
2020-08-21 15:42:37 -0400

    Child routing added on home + admin to home (home1, home2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { AboutComponent } from './components/about/about.component';
 import { AppRouting } from './app.routing';
+import { Home1Component } from './components/home1/home1.component';
+import { Home2Component } from './components/home2/home2.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { AppRouting } from './app.routing';
     HomeComponent,
     AdminComponent,
     AboutComponent,
+    Home1Component,
+    Home2Component,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { Home1Component } from './components/home1/home1.component';
+import { Home2Component } from './components/home2/home2.component';
 import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
 
 
 const routes: Routes = [
-	{ path: 'home', component: HomeComponent },
+	{ 
+		path: 'home',
+		component: HomeComponent,
+		children: [
+			{ path: 'home1', component: Home1Component },
+			{ path: 'home2', component: Home2Component }
+		] 
+	},
 	{ path: 'admin', component: AdminComponent },
 	{ path: 'about', component: AboutComponent }
 ];
