ab3ba35c809d050f6737fced5d74d817a9e92c7a
Tincho <tincholaguna37@gmail.com>
2020-08-25 14:52:27 -0400

    Installed Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { Home1Component } from './components/home1/home1.component';
 import { Home2Component } from './components/home2/home2.component';
 
 import { AppRouting } from './app.routing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { AppRouting } from './app.routing';
     BrowserModule,
     FormsModule,
     FruitsModule,
-    AppRouting
+    AppRouting,
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
