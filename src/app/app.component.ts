784850e90623eafff23ac8415c8835e1522ed234
Tincho <tincholaguna37@gmail.com>
2020-08-28 15:42:59 -0400

    Firebase works, implemented ngBootsrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { from, fromEvent } from 'rxjs';
 
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [{
+	type: 'success',
+	message: "ng Bootstrap is working!"
+}, {
+	type: 'info',
+	message: 'Here is another alert!'
+}]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'curso-angular';
 
+  alerts: Alert[];
+
+  constructor() {
+    this.reset();
+  }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset() {
+    this.alerts = Array.from(ALERTS);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { AppRouting } from './app.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +33,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FruitsModule,
     AppRouting,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
