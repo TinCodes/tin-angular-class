4ee8d1e5e4e5dd6cae6162aa419e2129319e3f2a
Tincho <tincholaguna37@gmail.com>
2020-08-28 16:18:25 -0400

    Entregas Ahora project started with structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-import { FruitsModule } from './modules/fruits/fruits.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { AboutComponent } from './components/about/about.component';
-import { Home1Component } from './components/home1/home1.component';
-import { Home2Component } from './components/home2/home2.component';
 
 import { AppRouting } from './app.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -20,21 +14,14 @@ import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AdminComponent,
-    AboutComponent,
-    Home1Component,
-    Home2Component,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    FruitsModule,
     AppRouting,
     NgbModule,
     BrowserAnimationsModule,
-    MatSliderModule,
     MatIconModule
   ],
   providers: [],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,24 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { Home1Component } from './components/home1/home1.component';
-import { Home2Component } from './components/home2/home2.component';
-import { AboutComponent } from './components/about/about.component';
-import { AdminComponent } from './components/admin/admin.component';
-
 
 const routes: Routes = [
-	{ 
-		path: 'home',
-		component: HomeComponent,
-		children: [
-			{ path: 'home1', component: Home1Component },
-			{ path: 'home2', component: Home2Component }
-		] 
-	},
-	{ path: 'admin', component: AdminComponent },
-	{ path: 'about', component: AboutComponent },
-	{ path: 'fruits', loadChildren: () => import('./modules/fruits/fruits.module').then(m => m.FruitsModule) }
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+	{ path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)},
+	{ path: 'list', loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule)}
 ];
 
 @NgModule({
