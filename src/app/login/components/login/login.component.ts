32df02bb5b9db187e09710add6a9e80fcc5de648
Tincho <tincholaguna37@gmail.com>
2020-09-08 15:39:59 -0400

    Modified login, implemented auth service to login with password

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,24 +8,27 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-	username = '';
-	password = '';
-
-	sw = true;
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  onLogin() : void {
-  	console.log('User: ', this.username);
-  	console.log('Password: ', this.password);
+  onLogin(form: any) : void {
+  	console.log('Form: ', form.value);
+
+    this.authService.login(form.value).subscribe(
+      res => {
+        console.log('Login Response: ', res);
+      },
+      err => {
+        console.log('Login Error: ', err);
+      }
+    );
   }
 
-  onAltLogin(form) : void {
-  	console.log('Local Form: ', form.value);
-  	this.router.navigate(['/pages']);
-  }
+  // onAltLogin(form) : void {
+  // 	console.log('Local Form: ', form.value);
+  // 	this.router.navigate(['/pages']);
+  // }
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,8 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NgElseDirective } from '../directives/ng-else.directive';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+
+import { AuthService } from '../services/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +24,14 @@ import { NgElseDirective } from '../directives/ng-else.directive';
     CommonModule,
     LoginRoutingModule,
     FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [
+    AuthService
   ]
 })
 export class LoginModule { }
